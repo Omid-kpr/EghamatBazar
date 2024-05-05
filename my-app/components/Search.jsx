547da5deff6cf26e3fec0bc3a0c8794958af2059
omid <95055060+Omid-kpr@users.noreply.github.com>
2024-05-05 23:52:18 +0330

    first version of search component

--- my-app/components/Search.jsx
+++ my-app/components/Search.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Input } from './ui/input'
 import { FaSearch } from 'react-icons/fa'
+import { IconContext } from "react-icons";
+
+
 const Search = () => {
+    const style = { color: "gray", fontSize: "1.25em" }
     return (
         <div className="relative flex  justify-center my-2 items-center">
-            <Input className="bg-gray-100 w-[90%] text-end placeholder:text-gray-600 placeholder:mr-6" placeholder="جستجوی اقامتگاه ها" />
-            <FaSearch className="absolute right-8 h-6 w-6 text-gray-600 text-xs" />
+            <Input
+                className="bg-gray-100 w-[90%] text-end text-sm placeholder:text-gray-600 pr-10"
+                placeholder="جستجوی اقامتگاه ها" />
+            <FaSearch className="absolute right-9" style={style} />
         </div>
 
     )
